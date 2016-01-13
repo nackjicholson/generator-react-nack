6afdfb95549c66153a1d9c6ae6551727177fae03
Will Vaughn <willieviseoae@gmail.com>
2016-01-12 16:33:12 -0800

    typo fixes.

--- app/templates/src/_component.test.js
+++ app/templates/src/_component.test.js
@@ -1,11 +1,18 @@
 import $ from 'teaspoon';
 import assert from 'assert';
 import React from 'react';
-import <%= classComponentName %> from './<%= classComponentName %>';
+import <%= camelComponentName %> from './<%= camelComponentName %>';
+
+function createComponent(customProps = {}) {
+  const props = Object.assign({}, customProps);
+  const <%= classComponentName %> = <%= camelComponentName %>(React);
+  return <<%= classComponentName %> {...props} />;
+}
 
 describe('<%= classComponentName %>', () => {
   it('should render Hello, World! h1 by default', () => {
-    const actual = $(<<%= classComponentName %> />)
+    const component = createComponent();
+    const actual = $(component)
       .render()
       .find('h1')
       .text();
@@ -16,8 +23,9 @@ describe('<%= classComponentName %>', () => {
 
   it('should render custom greeting prop', () => {
     const props = { greeting: 'test.greeting' };
+    const component = createComponent(props);
 
-    const actual = $(<<%= classComponentName %> {...props} />)
+    const actual = $(component)
       .render()
       .find('h1')
       .text();
@@ -28,8 +36,9 @@ describe('<%= classComponentName %>', () => {
 
   it('should render custom message prop', () => {
     const props = { message: 'test.message' };
+    const component = createComponent(props);
 
-    const actual = $(<<%= classComponentName %> {...props} />)
+    const actual = $(component)
       .render()
       .find('h1')
       .text();
